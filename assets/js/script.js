fae0dd5e06b45099a618a67ba33bf769d78e5b3f
lhafkamp <Luuk@111.155.028.145.hva.nl>
2017-03-07 11:50:39 +0100

    filtering on place > music works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,40 @@
-// object API
-
-// fetch(`http://funda.kyrandia.nl/feeds/Aanbod.svc/json/detail/${APIKEY}/koop/643335c8-380f-4350-b0df-dff5b6da1573/`)
-// 	.then(data => data.json())
-// 	.then(data => console.log(data))
-
-
-// search API
-
-const body = document.querySelector('body');
+const houses = document.querySelector('.houses');
 const results = [];
+const places = ['amsterdam', 'amstelveen', 'diemen'];
+let placeChoice = '';
+const placeButtons = document.querySelectorAll('.cities button');
+const choiceButtons = document.querySelectorAll('button');
 
-fetch(`http://funda.kyrandia.nl/feeds/Aanbod.svc/json/${APIKEY}/?type=koop&zo=/diemen/&page=1&pagesize=25`)
-	.then(data => data.json())
-	.then(data => results.push(...data.Objects))
-	.then(data => showData());
+
+function getHousesByQuery(city, music) {
+	console.log(city, music);
+	fetch(`http://funda.kyrandia.nl/feeds/Aanbod.svc/json/${APIKEY}/?type=huur&zo=/${city}${music}/&page=1&pagesize=25`)
+		.then(data => data.json())
+		.then(data => results.push(...data.Objects))
+		.then(data => showData());
+}
+
+function selectPlace() {
+	places.forEach((place) => {
+		if (this.textContent === place) {
+			placeChoice = this.textContent;
+		}
+	});
+}
+
+function selectChoice() {
+	if (this.textContent === 'muzikant') {
+		getHousesByQuery(placeChoice, '/garage');
+	}
+}
+
+choiceButtons[3].addEventListener('click', selectChoice);
+placeButtons.forEach(button => button.addEventListener('click', selectPlace));
 
 
 function showData() {
 	const newResults = results
-		// .map(data => console.log(data.WGS84_Y))
+		// .map(data => console.log(data))
 		.map(data => mainDom(data))
 		.join('');
 	renderToDom(newResults);
@@ -30,31 +46,12 @@ function mainDom(house) {
 			<img src="${house.FotoMedium}">
 			<p>${house.Adres}</p>
 			<p>${house.AangebodenSindsTekst}</p>
-			<p>${house.PrijsGeformatteerdHtml}</p>
+			<p>€${house.Prijs.Huurprijs}</p>
 		</div>
 	`;
 }
 
 function renderToDom(elements) {
-	body.innerHTML = elements;
+	houses.innerHTML = elements;
 }
-
-const geo = navigator.geolocation;
-let coords = '';
-
-const getCoords = function (pos) {
-	
-	let x = pos.coords.latitude;
-	let y = pos.coords.longitude;
-
-	coords =  {
-		x:x,
-		y:y
-	};
-}; 
-
-geo.getCurrentPosition(getCoords);
-
-
-
 
